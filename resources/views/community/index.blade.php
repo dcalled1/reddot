716ec445d7b7ab1b7fe10b6508bffd5568587aa6
samorenoq <samorenoq@eafit.edu.co>
2020-09-15 20:44:18 -0500

    CommunityController done

---
+++ resources/views/community/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.base')
+
+@section("title", $data["title"])
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @include('util.message')
+            <div class="card">
+                <div class="card-header d-flex">
+                Communities
+                <a href="{{ route('community.create') }}" class="ml-auto btn btn-primary">Create Community</a>
+                
+                </div>
+
+                <div class="card-body">
+                    <div class="row">
+                        <div class="table-responsive-lg col-12">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Topics</th>
+                                        <th>Tags</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($data["post"] as $p)
+                                            <tr>
+                                                <td><b><a href="#">{{ $p->getTitle() }}</a></b></td>
+                                                <td>{{ $p->getTopics() }}</td>
+                                                <td>{{ $p->getTags() }}</td>
+                                            </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
