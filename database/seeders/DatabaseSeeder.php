fafdefd8a215db5bdaca8050d04dc1cf5659059f
David Calle Daza <dcalled1@eafit.edu.co>
2020-11-10 21:31:45 -0500

    Seeders ready

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Announcement;
+use App\Models\Comment;
+use App\Models\Community;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +18,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        User::factory()->count(20)->create();
+        Community::factory()->count(8)->create();
+        $posts = Post::factory()->count(60)->create();
+        $announcements = Announcement::factory()->count(30)->create();
+        $comments = Comment::factory()->count(200)->create();
+        foreach($posts as $post) {
+            $post->community->addMember($post->author);
+        }
+        foreach($announcements as $announcement) {
+            $announcement->community->addMember($announcement->author);
+        }
+        foreach($comments as $comment) {
+            $comment->post->community->addMember($comment->author);
+        }
     }
 }
