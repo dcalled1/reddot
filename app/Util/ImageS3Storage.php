069bae7768ba13f5ee06ac200afffb07c0aa1ecd
samorenoq <samorenoq@eafit.edu.co>
2020-11-10 22:36:26 -0500

    Inversión de dependencias

---
+++ app/Util/ImageS3Storage.php
@@ -0,0 +1,22 @@
+<?php 
+
+    namespace App\Util;
+
+    use App\Interfaces\ImageStorage;
+    use Illuminate\Support\Facades\Storage;
+
+    class ImageS3Storage implements ImageStorage
+    {
+        public function store($request)
+        {
+            if ($request->hasFile('post_image'))
+            {
+                $file = $request->file('post_image');
+                $name = time() . $file->getClientOriginalName();
+                $filePath = 'images/' . $name;
+                Storage::disk('s3')->put($filePath, file_get_contents($file));
+            }
+        }
+    }
+
+?>
